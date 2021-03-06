f32783c98f338eabdd75ced5f3926764ae00c200
Marcos Camargo <marcoscamargo@outlook.com>
2021-03-06 00:42:32 -0300

    Percentage functin added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const numbers = document.getElementsByClassName("numbers");
 const output = document.querySelector('.output');
-const sum = document.querySelector('.sum');
+const adition = document.querySelector('.adition');
 const subtraction = document.querySelector('.subtraction');
 const multiplication = document.querySelector('.multiplication');
 const division = document.querySelector('.division');
+const percentage = document.querySelector('.percentage');
 const equal = document.querySelector('.equal');
 const clear = document.querySelector('.clear');
 var result = 0;
@@ -11,10 +12,12 @@ var isSubtraction;
 var isAdition;
 var isMultiplication;
 var isDivision;
+var isPercentage;
+var prevNumber;
 
 
 
-
+// All the events listener
 for (let index = 0; index < numbers.length; index++) {
     numbers[index].addEventListener('click', () => {
         addEventListener('click', compute(numbers[index].innerText)); 
@@ -22,71 +25,48 @@ for (let index = 0; index < numbers.length; index++) {
     
 }
 
-sum.addEventListener('click', add);
+adition.addEventListener('click', add);
 subtraction.addEventListener('click', sub);
 division.addEventListener('click', div);
 multiplication.addEventListener('click', mult);
+percentage.addEventListener('click', percentageFuntion);
 clear.addEventListener('click', clearFunction);
 equal.addEventListener('click', equalFunction);
 
-
-
-
 function compute(num){
-    output.innerText += num;    
-    
+    output.innerText += num;       
+
 };
 
-function add(){
-    
+function add(){    
     result += Number(output.innerText);
-
-    output.innerText = "";
-    console.log(output.innerText);
-    
-
-    console.log(result);
-
+    output.innerText = ""; 
     isAdition = true;
 };
 
-function sub(){
-    
+function sub(){    
     result += Number(output.innerText);
-
-    output.innerText = "";
-    console.log(output.innerText);
-    
-
-    console.log(result);
-
+    output.innerText = "";  
     isSubtraction = true;
 };
 
-function div(){
-    
+function div(){    
     result += Number(output.innerText);
-
-    output.innerText = "";
-    console.log(output.innerText);
-    
-
-    console.log(result);
-
+    output.innerText = "";   
     isDivision = true;
 };
 
-function mult(){
-    
+function mult(){    
     result += Number(output.innerText);
-
     output.innerText = "";
-    console.log(output.innerText);
-    
+    isPercentage = true;
+};
 
-    console.log(result);
-
-    isMultiplication = true;
+function percentageFuntion(){    
+    prevNumber = Number(output.innerText);
+    result += Number(output.innerText);
+    output.innerText = "";
+    isPercentage = true;
 };
 
 function equalFunction(){
@@ -113,21 +93,20 @@ function equalFunction(){
         console.log(result);       
         output.innerText = result;
         isMultiplication = false;
-
     }
-   
-
+    else if(isPercentage === true){
+        result *= (parseFloat(output.innerText) / 100);
+        console.log("sdd", result);   
+        console.log(output.innerText);    
+        output.innerText = `${prevNumber}% de ${output.innerText} = ${result}`;
+        isPercentage = false;
+    }
     result = 0;
-    
-
-
 }
 
 function clearFunction(){    
     output.innerText = "";   
-    result = 0;
-
-   
+    result = 0;   
 };
 
 
